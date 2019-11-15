746b07dd864aa427116ba41e5c4ccd68edb550c8
Abhishek Rao <abhishekrao@abhishek-Appss-Mac-mini.local>
2019-11-15 12:05:55 +0530

    working forns

--- App.js
+++ App.js
@@ -1,44 +1,109 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, Button, View } from "react-native";
+import { StyleSheet, Button, View, Text } from "react-native";
+import t from 'tcomb-form-native';
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#F5FCFF"
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: "center",
+//     margin: 10
+//   },
+//   instructions: {
+//     textAlign: "center",
+//     color: "#333333",
+//     marginBottom: 5
+//   }
+// });
 
+handleSubmit = () => {
+  const value = this._form.getValue(); // use that ref to get the form value
+  console.log('value: ', value);
+}
 
+addReactiveForm = () => {
+    return User;
+}
 
+const Form = t.form.Form;
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+  terms: t.Boolean
+});
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
 });
-
 export default class App extends Component {
-
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
+      // <Button
+      //   title="Go to Details"
+      //   onPress={() => this.props.navigation.navigate('Home')}
+      // />
+      // <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      //   <UserInput
+      //     source={usernameImg}
+      //     placeholder="Username"
+      //     autoCapitalize={'none'}
+      //     returnKeyType={'done'}
+      //     autoCorrect={false}
+      //   />
+      //   <UserInput
+      //     source={passwordImg}
+      //     secureTextEntry={this.state.showPass}
+      //     placeholder="Password"
+      //     returnKeyType={'done'}
+      //     autoCapitalize={'none'}
+      //     autoCorrect={false}
+      //   />
+      //   <TouchableOpacity
+      //     activeOpacity={0.7}
+      //     style={styles.btnEye}
+      //     onPress={this.showPass}>
+      //     <Image source={eyeImg} style={styles.iconEye} />
+      //   </TouchableOpacity>
+      // </KeyboardAvoidingView>
+
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Navigation Sample!
-        </Text>
+        <Form
+          ref={c => this._form = c} // assign a ref
+          type={User}
+        />
         <Button
-            title="Go to Details"
-            onPress={() => this.props.navigation.navigate('Home')}
-          />
-        {/* <Button
-          onPress={() => navigate("SecondScreen")}
-          title="Go to Second Screen"
-        /> */}
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
       </View>
     );
   }
 }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//   },
+//   btnEye: {
+//     position: 'absolute',
+//     top: 55,
+//     right: 28,
+//   },
+//   iconEye: {
+//     width: 25,
+//     height: 25,
+//     tintColor: 'rgba(0,0,0,0.2)',
+//   },
+// });
