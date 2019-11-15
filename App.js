7d90a64087685d8ecc878586cf21eab8b3ceff56
Abhishek Rao <abhishekrao@abhishek-Appss-Mac-mini.local>
2019-11-15 16:12:18 +0530

    working fornghfghfs

--- App.js
+++ App.js
@@ -1,41 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Button, View, Text } from "react-native";
-import t from 'tcomb-form-native';
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#F5FCFF"
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: "center",
-//     margin: 10
-//   },
-//   instructions: {
-//     textAlign: "center",
-//     color: "#333333",
-//     marginBottom: 5
-//   }
-// });
-
-handleSubmit = () => {
-  const value = this._form.getValue(); // use that ref to get the form value
-  console.log('value: ', value);
-}
-
-addReactiveForm = () => {
-    return User;
-}
-
-const Form = t.form.Form;
-const User = t.struct({
-  email: t.String,
-  username: t.String,
-  password: t.String,
-  terms: t.Boolean
-});
+import { StyleSheet, Button, View, Text, FlatList } from "react-native";
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
@@ -44,9 +8,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
 });
+
 export default class App extends Component {
   constructor(props) {
     super(props)
+    this.state = { isLoading: true }
+  }
+  componentWillMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function () {
+        });
+      }).catch((error) => {
+        console.error(error);
+      });
   }
   render() {
     return (
@@ -54,56 +33,27 @@ export default class App extends Component {
       //   title="Go to Details"
       //   onPress={() => this.props.navigation.navigate('Home')}
       // />
-      // <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      //   <UserInput
-      //     source={usernameImg}
-      //     placeholder="Username"
-      //     autoCapitalize={'none'}
-      //     returnKeyType={'done'}
-      //     autoCorrect={false}
+      // <View style={styles.container}>
+      //   {/* <Form
+      //     ref={c => this._form = c} // assign a ref
+      //     type={User}
+      //   /> */}
+      //   <Button
+      //     title="Sign Up!"
+      //     onPress={() => this.props.navigation.navigate('Home')}
       //   />
-      //   <UserInput
-      //     source={passwordImg}
-      //     secureTextEntry={this.state.showPass}
-      //     placeholder="Password"
-      //     returnKeyType={'done'}
-      //     autoCapitalize={'none'}
-      //     autoCorrect={false}
+      //   <Button
+      //     title="getMoviesFromApi"
+      //     onPress={() => this.getMoviesFromApi()}
       //   />
-      //   <TouchableOpacity
-      //     activeOpacity={0.7}
-      //     style={styles.btnEye}
-      //     onPress={this.showPass}>
-      //     <Image source={eyeImg} style={styles.iconEye} />
-      //   </TouchableOpacity>
-      // </KeyboardAvoidingView>
-
-      <View style={styles.container}>
-        <Form
-          ref={c => this._form = c} // assign a ref
-          type={User}
-        />
-        <Button
-          title="Sign Up!"
-          onPress={this.handleSubmit}
+      // </View>
+      <View style={{ flex: 1, padding: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({ id }, index) => id}
         />
       </View>
     );
   }
 }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//   },
-//   btnEye: {
-//     position: 'absolute',
-//     top: 55,
-//     right: 28,
-//   },
-//   iconEye: {
-//     width: 25,
-//     height: 25,
-//     tintColor: 'rgba(0,0,0,0.2)',
-//   },
-// });
