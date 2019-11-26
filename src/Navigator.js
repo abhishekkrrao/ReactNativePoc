4ce2877744a10c473864f93da2d6f4cdcd24ed15
Abhishek Rao <abhishekrao@abhishek-Appss-Mac-mini.local>
2019-11-26 11:59:48 +0530

    working on bottom, icns

--- src/Navigator.js
+++ src/Navigator.js
@@ -3,15 +3,75 @@ import Home from './home'
 import App from './../App'
 import Modals from './modal'
 import Loading from './Loading'
+import { Image } from "react-native";
 import { createAppContainer, StackActions, NavigationActions } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { from } from "rxjs";
 const TabNavigator = createBottomTabNavigator({
-
-    Home: { screen: Home },
-    App: { screen: App },
-    Modals: { screen: Modals },
-})
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            title: "Home",
+            headerLeft: null,
+            gesturesEnabled: false,
+            header: {
+                visible: false,
+                left: null,
+            },
+        }
+    },
+    App:
+    {
+        screen: App,
+        navigationOptions: {
+            title: "Settings",
+            headerLeft: null,
+            gesturesEnabled: false,
+            header: {
+                visible: false,
+                left: null,
+            },
+        }
+    },
+    Modals:
+    {
+        screen: Modals,
+        navigationOptions: {
+            title: "Contacts",
+            headerLeft: null,
+            gesturesEnabled: false,
+            header: {
+                visible: false,
+                left: null,
+            },
+        }
+    },
+},
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                if (routeName === 'Home') {
+                    return (
+                        <Image
+                            source={require('./assets/home.png')}
+                            style={{ width: 20, height: 20 }} />
+                    );
+                } else {
+                    return (
+                        <Image
+                            source={require('./assets/settings.png')}
+                            style={{ width: 20, height: 20 }} />
+                    );
+                }
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: '#FF6F00',
+            inactiveTintColor: '#263238',
+        },
+    })
 
 const AppNavigator = createStackNavigator({
     Loading: { screen: Loading },
